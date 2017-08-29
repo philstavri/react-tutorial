ed7491abed6eb64732d2f49dbf46452a4f0866cb
Phil Stavri <phil.stavri@capita.co.uk>
2017-08-29 16:42:43 +0100

    Extension show coordinates of change in move list

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Board from "../board/Board";
 
 class Game extends React.Component {
@@ -8,15 +8,15 @@ class Game extends React.Component {
 
         this.state = {
             history: [{
-                squares: Array(9).fill(null),
+                squares: Array(9).fill(null)
             }],
             stepNumber: 0,
-            xIsNext: true,
+            xIsNext: true
         };
     }
 
     getNextMove() {
-        return this.state.xIsNext ? 'X' : 'O';
+        return this.state.xIsNext ? "X" : "O";
     }
 
     handleClick(i) {
@@ -24,45 +24,52 @@ class Game extends React.Component {
         let current = history[this.state.stepNumber];
         let squares = current.squares.slice();
 
-        if( calculateWinner(squares) || squares[i]){
+        if (current.winner || squares[i]) {
             return;
         }
 
         squares[i] = this.getNextMove();
+        let winner = calculateWinner(squares);
         this.setState({
-            history: history.concat([{squares}]),
+            history: history.concat([{ squares, winner }]),
             stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
+            xIsNext: !this.state.xIsNext
         });
     }
 
     jumpTo(step) {
-        console.log("step", step);
         this.setState({
             stepNumber: step,
-            xIsNext: (step % 2) === 0,
+            xIsNext: (step % 2) === 0
         });
     }
 
     render() {
-
         let history = this.state.history.slice();
         let current = history[this.state.stepNumber];
-        let winner = calculateWinner(current.squares);
+        let winner = current.winner;
 
-        if(this.state.reverseMoves) {
+        if (this.state.reverseMoves) {
             history.reverse();
         }
 
         let moves = history.map((step, move) => {
 
-            if(this.state.reverseMoves) {
+            let prevStep = history[move - 1];
+            if (this.state.reverseMoves) {
+                prevStep = history[move + 1];
                 move = history.length - (move + 1);
             }
 
-            let desc = move ?
-                'Move #' + move :
-                'Game start';
+            let desc = "Game start";
+
+            if(move && prevStep) {
+                let coordinatesOfChange = getCoordinatesOfChange(step.squares, prevStep.squares);
+                if(coordinatesOfChange){
+                    desc = `Move (${coordinatesOfChange.col},${coordinatesOfChange.row})`;
+                }
+            }
+
             return (
                 <li key={move}>
                     <a href="#" onClick={() => this.jumpTo(move)}>
@@ -79,8 +86,8 @@ class Game extends React.Component {
 
         let status;
 
-        if(winner) {
-            status = `Winner ${winner}`;
+        if (winner) {
+            status = `Winner ${winner.player}`;
         } else {
             status = `Next player: ${this.getNextMove()}`;
         }
@@ -90,13 +97,13 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board
                         squares={current.squares}
-                        onClick={(i)=>this.handleClick(i)}/>
+                        onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
                     <div>{ status }</div>
                     <ol>{moves}</ol>
                 </div>
-                <button onClick={()=>this.toggleMovesList()}>Toggle moves</button>
+                <button onClick={() => this.toggleMovesList()}>Toggle moves</button>
             </div>
         );
     }
@@ -108,6 +115,20 @@ class Game extends React.Component {
     }
 }
 
+function getCoordinatesOfChange(newSquares, oldSquares) {
+
+    if(newSquares && oldSquares && newSquares.length === oldSquares.length){
+        for (let i in newSquares) {
+            if(newSquares[i] && newSquares[i] !== oldSquares[i]){
+                let row = Math.floor(i / 3) + 1;
+                let col = (i % 3) + 1;
+                return { row, col };
+            }
+        }
+    }
+
+}
+
 function calculateWinner(squares) {
     const lines = [
         [0, 1, 2],
@@ -117,12 +138,15 @@ function calculateWinner(squares) {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6],
+        [2, 4, 6]
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return {
+                player: squares[a],
+                positions: [a, b, c]
+            };
         }
     }
     return null;
