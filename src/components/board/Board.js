cc123e098516929d21cff6b1acccefd4b679a513
Phil Stavri <phil.stavri@capita.co.uk>
2017-08-29 16:42:08 +0100

    Extension refactored board to use loops rather than hardcoding elements

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -6,6 +6,7 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                key={i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
 
@@ -16,23 +17,26 @@ class Board extends React.Component {
 
     render() {
 
+        let gridSize = 3;
+        let squareIndex = 0;
+        let rows = [];
+
+        for( let i = 0; i < gridSize; i++){
+            let cols = [];
+            for( let j = 0; j < gridSize; j++){
+                cols.push(this.renderSquare(squareIndex++))
+            }
+
+            rows.push(
+                <div className="board-row" key={i}>
+                    {cols}
+                </div>
+            );
+        }
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {rows}
             </div>
         );
     }
